bfac164cbc79947c484585f4e5bf322cb0e2a3f9
hinoja <hinoja2@gmail.com>
2024-06-26 19:13:22 +0200

    in progress

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->name('dashboard');
+
+
+
+// PROFILE
+Route::middleware(['auth', 'role:1'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+require __DIR__ . '/auth.php';
