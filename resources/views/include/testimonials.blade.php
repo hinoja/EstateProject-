6cca89d5baf75aebf03b7a8bd4d3398c95d1bbcc
hinoja <hinoja2@gmail.com>
2024-06-29 14:44:05 +0200

    update this branch with testing and tsafack

---
+++ resources/views/include/testimonials.blade.php
@@ -0,0 +1,109 @@
+<div class="swiper-wrapper">
+    <div class="swiper-slide">
+        <div class="box-tes-item wow fadeIn" data-wow-delay=".2s" data-wow-duration="2000ms">
+            <ul class="list-star">
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+            </ul>
+            <p class="text-justify fs-6 note body-1">
+                "J'ai acheté ma maison grâce à cette agence et je les recommande vivement. Leur
+                équipe a fait preuve d'un professionnalisme exceptionnel, comprenant mes besoins et
+                me guidant avec patience. Leur connaissance du marché local et leur réactivité ont
+                rendu le processus fluide. Je suis ravi de ma maison et remercie l'équipe de
+                Well-done pour leur excellent service." </p>
+            <div class="box-avt d-flex align-items-center gap-12">
+                <div class="avatar avt-60 round">
+                    <img src="{{ asset('assets/images/agents/agent1.jpg') }}" alt="avatar">
+                </div>
+                <div class="info">
+                    <div class="h7 fw-7">Femi Adebayo</div>
+                    <p class="text-variant-1 mt-4">Responsable Marketing</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="swiper-slide">
+        <div class="box-tes-item wow fadeIn" data-wow-delay=".4s" data-wow-duration="2000ms">
+            <ul class="list-star">
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+            </ul>
+            <p class="text-justify fs-6 note body-1">
+                "Je tiens à saluer l'excellent service de cette agence lors de mon achat immobilier.
+                Ils ont été à l'écoute de mes besoins et m'ont guidé vers la bonne propriété. Ils
+                ont
+                géré tous les aspects techniques et juridiques avec professionnalisme, rendant
+                l'acquisition fluide et sans accroc."
+            </p>
+            <div class="box-avt d-flex align-items-center gap-12">
+                <div class="avatar avt-60 round">
+                    <img src="{{ asset('assets/images/agents/agent5.jpg') }}" alt="avatar">
+                </div>
+                <div class="info">
+                    <div class="h7 fw-7">Jean-Pierre Ngassa</div>
+                    <p class="text-variant-1 mt-4">Directeur Hospitalier</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="swiper-slide">
+        <div class="box-tes-item wow fadeIn" data-wow-delay=".6s" data-wow-duration="2000ms">
+            <ul class="list-star">
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+            </ul>
+            <p class="text-justify fs-6 note body-1">
+                "Mon expérience avec les services de gestion immobilière a été
+                exceptionnelle. Ils gèrent les propriétés de manière professionnelle et réactive, et
+                résolvent
+                rapidement tout problème. Leur approche attentive me donne une grande tranquillité
+                d'esprit."
+            </p>
+            <div class="box-avt d-flex align-items-center gap-12">
+                <div class="avatar avt-60 round">
+                    <img src="{{ asset('assets/images/agents/agent2.jpg') }}" alt="avatar">
+                </div>
+                <div class="info">
+                    <div class="h7 fw-7">Abebe Bekele</div>
+                    <p class="text-variant-1 mt-4">Chef de Projet</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="swiper-slide wow fadeIn" data-wow-delay=".6s" data-wow-duration="2000ms">
+        <div class="box-tes-item wow fadeIn" data-wow-delay=".2s" data-wow-duration="2000ms">
+            <ul class="list-star">
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+                <li class="icon icon-star"></li>
+            </ul>
+            <p class=" text-justify fs-6 note body-1">
+                "Je suis très satisfait du professionnalisme et des connaissances de l'équipe de
+                courtage. Ils m'ont aidé à trouver la maison idéale et m'ont soutenu dans les
+                aspects juridiques et financiers, me permettant de prendre ma décision en toute
+                confiance."
+            </p>
+            <div class="box-avt d-flex align-items-center gap-12">
+                <div class="avatar avt-60 round">
+                    <img src="{{ asset('assets/images/agents/agent4.jpg') }}" alt="avatar">
+                </div>
+                <div class="info">
+                    <div class="h7 fw-7">Nneka Okeke</div>
+                    <p class="text-variant-1 mt-4">Responsable de Communication</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
